de8ca922db1704ad3169ab3f5ef69670e3ec131b
dfilitto <danilo.filitto@gmail.com>
2021-08-04 13:40:19 -0300

    Recursos

---
+++ 02 Rotas/sitepessoal/resources/views/home.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Home</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="./css/style.css">
+
+
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <a href="#" class="branch"><img src="logo.png" alt="Minha logo" title="Nome da minha logo"></a>
+                <nav class="menu">
+                    <ul>
+                        <li><a class="menu-link" href="#">Início</a></li>
+                        <li><a class="menu-link" href="#">Contato</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <main class="container">
+            <article class="super-card">
+                <section class="super-card-img">
+                    <img src="danilofilitto.jpg" alt="Foto do Danilo Filitto", title="Danilo Filitto">
+                    <h2>Danilo Filitto</h2>
+                </section>
+                <section class="super-card-text">
+                    <p>
+                        Meu nome é Danilo Filitto, sou Mestre em Ciência da Computação pela 
+                        Universidade Estadual de Maringá – UEM, Pós-Graduado em Redes de Computadores 
+                        e Comunicação de Dados pela Universidade do Estado do Paraná – UEL, Bacharel 
+                        em Ciência da Computação pela Universidade do Oeste Paulista – UNOESTE.
+                    </p>
+                    <p>
+                        Atuo na área acadêmica como professor desde 2006. 
+                        Atualmente leciono na UNOESTE (Universidade do Oeste Paulista) e no SENAC 
+                        (Serviço Nacional de Aprendizagem Comercial) da cidade de Presidente Prudente. 
+                        Além de professor sou desenvolvedor de jogos e palestrante.
+                    </p>
+                    <p>
+                        As Área de Pesquisa em qual atuo são: desenvolvimento de Softwares, 
+                        estrutura de dados, processamento de imagens digitais, 
+                        redes de computadores e desenvolvimento de jogos..
+                    </p>
+                </section>
+            </article>
+        </main>
+    </body>
+</html>
